79060c79cdf009dd5328f029f5d92bde6c8b7e70
Haritha Gonuguntla <Haritha@Harithas-MacBook-Pro.local>
2020-08-27 14:15:22 -0500

    adding index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+//import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//create a react component
+const App = () => {
+    return <h1>Hi there!</h1>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Take react component and show it on screen
+ReactDOM.render(<App />, document.getElementById('root'));
